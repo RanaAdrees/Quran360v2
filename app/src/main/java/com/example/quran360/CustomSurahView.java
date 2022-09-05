f0b52ad2b93acee9bda8f905f16febefd93c5f3a
Adrees <rana.adrees5890@gmail.com>
2022-09-05 12:20:42 +0500

    initial commit

---
+++ app/src/main/java/com/example/quran360/CustomSurahView.java
@@ -0,0 +1,14 @@
+package com.example.quran360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomSurahView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_surah_view);
+    }
+}
