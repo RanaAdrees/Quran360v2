323c38401b84612dbd67e18354f2fe478d05eb4c
Adrees <rana.adrees5890@gmail.com>
2022-09-06 19:49:23 +0500

    Adding new activity for dialog box which give option to select translation

---
+++ app/src/main/java/com/example/quran360v2/TranslationDialog.java
@@ -0,0 +1,14 @@
+package com.example.quran360v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TranslationDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translation_dialog);
+    }
+}
